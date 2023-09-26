16cbe94d708ee155f4d27eb9a711e04ec534aaa4
Deepu <deepuharry02@gmail.com>
2023-09-27 03:10:39 +0530

    Contrct completed

--- server/contracts/DAO.sol
+++ server/contracts/DAO.sol
@@ -4,43 +4,55 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract DAO is ERC20
 {
+    /*info
+
+    cost of 1 vote=50 DAO
+    1 ether=100 DAO tokens
+
+    info*/
+
+    //custom data types-start
     enum VotingStatus
     {
         notStarted, 
-        live,
-        ended
+        inProgress,
+        completed
     }
-
-    address public manager;
 
     struct Proposals
     {
         string proposal; 
+        string[] options;
         address proposer;
         uint threshold;
         VotingStatus votingStatus;
-        uint resultCount;
-        bool result;
+        uint highestValue;
+        uint[] results;
+        uint winningOption;
     }
+    //custom data types-end
 
-    //cost of 1 vote=50 DAO
-    //1 ether=100 DAO tokens
-    uint public tokensPerEther=100; 
+    //state variables-start
+    address payable public manager;
 
     uint public pollId;
 
+    //pollId=>Proposals
+    //contains all the info about the proposals
     mapping(uint=>Proposals) public proposals;
 
     //users[pollId]=votes cast 
     //stores how many votes were cast by a user for any particular poll 
     mapping(address=>mapping(uint=>uint)) public votesCast;
 
-    modifier onlyManager
-    {
-        require(msg.sender==manager,"Only the manager can call");
-        _;
-    }
+    //stores how much a user has staked
+    mapping(address=>uint) public stakedAmount;
 
+    //stores the time duraion when it was staked at
+    mapping(address=>uint) public stakedAt;
+    //state variables-end
+
+    //events-start
     event newProposalDetails
     (
         uint pollId,
@@ -58,24 +70,36 @@ contract DAO is ERC20
     (
         uint tokensPurchased 
     );
+    //events-end
+
+    modifier onlyManager
+    {
+        require(msg.sender==manager,"Only the manager can call");
+        _;
+    }
 
     constructor() ERC20("DecentralizedAutonomousOrganization","DAO")
     {
-        manager=msg.sender; 
+        manager=payable(msg.sender); 
     } 
     
-    function newProposal(string memory proposal) public returns(uint)
+    //Add new proposals 
+    function newProposal(string memory proposal,string[] memory options) public returns(uint)
     {
+        //costs 25 DAO tokens to add a poll
+        _transfer(msg.sender, manager, 25);
         pollId++;
         proposals[pollId]=Proposals
-        (
-            proposal,
-            msg.sender,
-            0,
-            VotingStatus.notStarted,
-            0,
-            false
-        );
+        ({
+            proposal:proposal,
+            options:options,
+            proposer:msg.sender,
+            threshold:0,
+            votingStatus:VotingStatus.notStarted,
+            highestValue:0,
+            results:new uint[](0),
+            winningOption:0
+        });
         emit newProposalDetails
         (
             pollId,
@@ -85,6 +109,7 @@ contract DAO is ERC20
         return pollId;
     }
 
+    //Set the threshold to win 
     function setThreshold(uint _pollId,uint threshold) public onlyManager 
     {
         proposals[_pollId].threshold=threshold; 
@@ -96,11 +121,13 @@ contract DAO is ERC20
     }
 
     //tokens should be bought in multiples of 100 for simplicity 
-    function checkTokensCostInEther(uint tokens) public view returns(uint)
+    function checkTokensCostInEther(uint tokens) public pure returns(uint)
     {
-        return tokens/tokensPerEther; 
+        //tokens/tokensPerEther
+        return tokens/100; 
     }
 
+    //tokens should be bought in multiples of 100 for simplicity 
     function purchaseDAOtokens(uint tokens) public payable 
     {
         _mint(msg.sender, tokens);
@@ -118,5 +145,80 @@ contract DAO is ERC20
     function checkVoteCost(uint _pollId) public view returns(uint)
     {
         return (((votesCast[msg.sender][_pollId])+1)**2)*50;
+    }
+
+    function proposalDetails(uint _pollId) public view returns(string memory,string[] memory)
+    {
+        return (proposals[_pollId].proposal,proposals[_pollId].options);
+    }
+
+    function changeVotingState(VotingStatus status,uint _pollId) public onlyManager
+    {
+        proposals[_pollId].votingStatus=status;
+    }
+
+    function checkVotingStatus(uint _pollId) public view returns(VotingStatus)
+    {
+        return proposals[_pollId].votingStatus;
+    }
+
+    function vote(uint amount,uint _pollId,uint option) public 
+    {
+        require(proposals[_pollId].votingStatus==VotingStatus.inProgress,"Voting isn't in progress");
+        require(amount==((((votesCast[msg.sender][_pollId])+1)**2)*50),"send the right amount to cast the vote");
+        require(balanceOf(msg.sender)>=amount,"insufficient balance");
+        votesCast[msg.sender][_pollId]++;
+        proposals[_pollId].results[option]++;
+        if(proposals[_pollId].results[option]>proposals[_pollId].highestValue)
+        {
+            proposals[_pollId].highestValue=proposals[_pollId].results[option];
+            proposals[_pollId].winningOption=option;
+        }
+        _transfer(msg.sender, manager, amount);
+    }
+
+    function viewPollResults(uint _pollId) public view returns(uint,uint)
+    {
+        return (proposals[_pollId].highestValue,proposals[_pollId].winningOption);
+    }
+
+    function transferEthToManager() public onlyManager
+    {
+        manager.transfer(address(this).balance);
+    }
+
+    function managerEthBalance() public view onlyManager returns(uint) 
+    {
+        return manager.balance; 
+    }
+
+    function stake(uint amount) public 
+    {
+        require(balanceOf(msg.sender)>=amount);
+        if(stakedAmount[msg.sender]>0)
+        {
+            claim();
+        }
+        _transfer(msg.sender, manager, amount);
+        stakedAmount[msg.sender]+=amount; 
+        stakedAt[msg.sender]=block.timestamp;
+    }
+
+    function claim() public
+    {
+        require(stakedAmount[msg.sender]>=0,"no stakes.nothing to claim");
+        uint currentBlockTimeStamp=block.timestamp;
+        uint rewards;
+        stakedAt[msg.sender]=currentBlockTimeStamp;
+        rewards=(stakedAmount[msg.sender])*(currentBlockTimeStamp-stakedAt[msg.sender])/3.154e7;
+        _mint(msg.sender, rewards);
+    }
+
+    function unStake(uint amount) public 
+    {
+        require(stakedAmount[msg.sender]>=amount,"stakes are less");
+        claim();
+        stakedAmount[msg.sender]-=amount;
+        _transfer(manager, msg.sender, amount);
     }
 }
